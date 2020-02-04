82d6be5eff0aa6598ce7806b357e53d5e8cca812
Mariya Pasheva <pashevam@outlook.com>
2020-02-03 23:45:45 -0600

    Matrix completed

---
+++ Matrix/main.cpp
@@ -0,0 +1,230 @@
+/************************************************
+ *  Title: CS251, Project 2 Matrix, UIC         *
+ *                                              *
+ * Author: Mariya Pasheva                       *
+ *                                              *
+ * System: Linux                                *
+ *                                              *
+ * Date: 02/01/2020                             *
+ *                                              *
+ * Purpose: Building and testing a matrix class *
+ *          with Catch2 framework.              *
+ *                                              *
+ * Usage:                                       *
+ * g++ -g  main.cpp -o program.exe              *
+ *        ./program.exe                         *
+************************************************/
+
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+
+#include "mymatrix.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <random>
+#include <chrono>
+#include <cmath>
+
+using namespace std;
+
+TEST_CASE("Default constructor")
+{
+    mymatrix<int> M;
+    REQUIRE(M.size() == 16);
+}
+
+TEST_CASE("Overloaded constructor")
+{
+    SECTION("More rows than columns")
+    {
+        mymatrix<int>M(10,3);
+        REQUIRE(M.size() == 30);
+    }
+    SECTION("More columns than rows")
+    {
+        mymatrix<int>M(2,10);
+        REQUIRE(M.size() == 20);
+    }
+}
+TEST_CASE("Teasting the deep copy constructor")
+{
+    mymatrix<int>M;
+    M.at(3,2) = 50;
+    mymatrix<int>M2 = M;
+
+    SECTION("Changing M2 should not reflect M")
+    {
+        M2.at(3,2) = 10;
+        REQUIRE(M2.at(3,2) == 10);
+        REQUIRE(M.at(3,2) == 50);
+    }
+}
+TEST_CASE("Testing the number of rows output")
+{
+    mymatrix<int>M;
+    mymatrix<int>M2(1,5);
+    mymatrix<int>M3(100,2);
+
+    REQUIRE(M.numrows() == 4);
+    REQUIRE(M2.numrows() == 1);
+    REQUIRE(M3.numrows() == 100);
+}
+TEST_CASE("Number of columns at a particular row")
+{
+    mymatrix<int>M;
+    mymatrix<int>M2(6,10);
+    mymatrix<int>M3(3,95);
+
+    SECTION("Changing pacritular row to a different number of columns")
+    {
+        M.growcols(1,15);
+        REQUIRE(M.numcols(1) == 15);
+    }
+
+    REQUIRE(M.numcols(0) == 4);
+    REQUIRE(M2.numcols(1) == 10);
+    REQUIRE(M3.numcols(2) == 95);
+}
+TEST_CASE("Growing a particular column")
+{
+    mymatrix<int> M;
+    mymatrix<int> M2(6, 9);
+
+    M.growcols(2,6);
+    M2.growcols(5,20);
+
+    REQUIRE(M.numcols(2) == 6);
+    REQUIRE(M2.numcols(5) == 20);
+}
+TEST_CASE("Testing the grow function")
+{
+    mymatrix<int>M;
+
+    SECTION("Grow both row and column")
+    {
+        M.grow(10,10);
+        REQUIRE(M.numrows() == 10);
+        REQUIRE(M.numcols(0) == 10);
+        REQUIRE(M.numcols(1) == 10);
+        REQUIRE(M.numcols(2) == 10);
+        REQUIRE(M.numcols(3) == 10);
+        REQUIRE(M.numcols(4) == 10);
+        REQUIRE(M.numcols(5) == 10);
+        REQUIRE(M.numcols(6) == 10);
+        REQUIRE(M.numcols(7) == 10);
+        REQUIRE(M.numcols(8) == 10);
+        REQUIRE(M.numcols(9) == 10);
+    }
+    SECTION("Grow only columns")
+    {
+        M.grow(4,7);
+        REQUIRE(M.numrows() == 4);
+        REQUIRE(M.numcols(0) == 7);
+        REQUIRE(M.numcols(1) == 7);
+        REQUIRE(M.numcols(2) == 7);
+        REQUIRE(M.numcols(3) == 7);
+    }
+    SECTION("Grow columns which are uneven")
+    {
+        M.growcols(1,15);
+        M.grow(4,9);
+        REQUIRE(M.numrows() == 4);
+        REQUIRE(M.numcols(0) == 9);
+        REQUIRE(M.numcols(1) == 15);
+        REQUIRE(M.numcols(2) == 9);
+        REQUIRE(M.numcols(3) == 9);
+    }
+}
+TEST_CASE("Testing whether we are returning the correct size")
+{
+    mymatrix<int>M;
+
+    REQUIRE(M.size() == 16);
+
+    SECTION("Different number of columns")
+    {
+        M.grow(10,10);
+        M.growcols(0,50);
+        M.growcols(9,15);
+        REQUIRE(M.size() == 145);
+    }
+}
+TEST_CASE("Accessing and altering element at row and column")
+{
+    mymatrix<int>M;
+
+    M.at(0,3) = 5;
+    REQUIRE(M.at(0,3) == 5);
+    M.at(0,3) *= 2;
+    REQUIRE(M.at(0,3) == 10);
+    M.growcols(3,10);
+    M.at(3,6) = 6;
+    REQUIRE(M.at(3,6) == 6);
+}
+TEST_CASE("Operator ()")
+{
+    mymatrix<int>M;
+
+    M(0,3) = 5;
+    REQUIRE(M(0,3) == 5);
+    M.at(0,3) *= 2;
+    REQUIRE(M(0,3) == 10);
+    M.growcols(3,10);
+    M(3,6) = 6;
+    REQUIRE(M(3,6) == 6);
+}
+TEST_CASE("Operator *")
+{
+    mymatrix<int>M1(2,2);
+    M1.at(0,0) = 1;
+    M1.at(0,1) = 2;
+    M1.at(1,0) = 3;
+    M1.at(1,1) = 4;
+    mymatrix<int>M2;
+    M2 = M1 * 3;
+    REQUIRE(M2.at(0,0) == 3);
+    REQUIRE(M2.at(0,1) == 6);
+    REQUIRE(M2.at(1,0) == 9);
+    REQUIRE(M2.at(1,1) == 12);
+}
+TEST_CASE("Testing matrix multiplication")
+{
+    mymatrix<int>M1(2,3);
+    mymatrix<int>M2(3,3);
+    //result is 2x3 matrix
+    mymatrix<int>M3;
+
+    //Setting M1 values;
+    M1.at(0,0) = 1;
+    M1.at(0,1) = 2;
+    M1.at(0,2) = 3;
+    M1.at(1,0) = 4;
+    M1.at(1,1) = 5;
+    M1.at(1,2) = 6;
+    //Stting M2 values;
+    M2.at(0,0) = 1;
+    M2.at(0,1) = 2;
+    M2.at(0,2) = 3;
+    M2.at(1,0) = 4;
+    M2.at(1,1) = 5;
+    M2.at(1,2) = 6;
+    M2.at(2,0) = 7;
+    M2.at(2,1) = 8;
+    M2.at(2,2) = 9;
+
+    M3 = M1 * M2;
+    //Testing size
+    REQUIRE(M3.size() == 6);
+    REQUIRE(M3.numrows() == 2);
+    REQUIRE(M3.numcols(0) == 3);
+    REQUIRE(M3.numcols(1) == 3);
+    //Testing values, multiplication.
+    REQUIRE(M3.at(0,0) == 30);
+    REQUIRE(M3.at(0,1) == 36);
+    REQUIRE(M3.at(0,2) == 42);
+    REQUIRE(M3.at(1,0) == 66);
+    REQUIRE(M3.at(1,1) == 81);
+    REQUIRE(M3.at(1,2) == 96);
+}
---
+++ Matrix/mymatrix.h
@@ -0,0 +1,553 @@
+/*mymatrix.h*/
+
+// 
+// Mariya Pasheva
+// U. of Illinois, Chicago
+// CS 251: Spring 2020
+// Project #02
+//
+
+//
+// mymatrix
+//
+// The mymatrix class provides a matrix (2D array) abstraction.
+// The size can grow dynamically in both directions (rows and 
+// cols).  Also, rows can be "jagged" --- i.e. rows can have 
+// different column sizes, and thus the matrix is not necessarily 
+// rectangular.  All elements are initialized to the default value
+// for the given type T.  Example:
+//
+//   mymatrix<int>  M;  // 4x4 matrix, initialized to 0
+//   
+//   M(0, 0) = 123;
+//   M(1, 1) = 456;
+//   M(2, 2) = 789;
+//   M(3, 3) = -99;
+//
+//   M.growcols(1, 8);  // increase # of cols in row 1 to 8
+//
+//   for (int r = 0; r < M.numrows(); ++r)
+//   {
+//      for (int c = 0; c < M.numcols(r); ++c)
+//         cout << M(r, c) << " ";
+//      cout << endl;
+//   }
+//
+//   Output:
+//   123 0 0 0
+//   0 456 0 0 0 0 0 0
+//   0 0 789 0
+//   0 0 0 -99
+//
+/*--------------------------------------------------------------------------------------*\
+ *                        NOTES
+ *                        ^^^^^
+ * 1. Throught the program in the loops variables r stands
+ *    for the row and c stands for the columns.
+ * 2. Private helper function copyMatrixVals(ROW*, ROW**) has
+ *    has been added.
+ * 3. Through the code I keep declaring and initializing the Columns to T{}
+ *    in order to prevent **Conditional jump or move depends on uninitialised value**
+ *    The error occurs when the content of a heap block (in our case the Cols array)
+ *    is written to before initialized to a certain value.
+\*--------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+
+using namespace std;
+
+template<typename T> // Currently accepting any type such as mymatrix<int> or  mymatrix<string> and etc.
+class mymatrix
+{
+private:
+  struct ROW
+  {
+    T*  Cols;     // dynamic array of column elements
+    int NumCols;  // total # of columns (0..NumCols-1)
+  };
+
+  ROW* Rows;     // dynamic array of ROWs. Pointer to the ROW struct.
+  int  NumRows;  // total # of rows (0..NumRows-1)
+
+  /**
+   * The function is copying over the T elements from
+   * one matrix to the other.
+   *
+   * @param fromStruct is copying over the address of the Rows pointer.
+   *                   since the elemtns in it won't be aletered.
+   * @param toStruct is a double pointer to the Rows, which allows us to
+   *                 pass the matrix by reference, (pass the addr),
+   *                 therefore we can write to the matrix and add the elements.
+   */
+  void copyMatrixVal(ROW* fromStruct, ROW** toStruct)
+   {
+       /* this->NumRows is the current num of rows in the fromStruct */
+       for (int r = 0; r < this->NumRows; ++r)
+       {
+           for (int c = 0; c < fromStruct[r].NumCols; ++c)
+           {
+               (*toStruct)[r].Cols[c] = fromStruct[r].Cols[c];
+           }
+       }
+   }
+
+public:
+  /*
+   * default constructor:
+   *
+   * Called automatically by C++ to construct a 4x4 matrix.
+   * All elements are initialized to the default value of T.
+   */
+  mymatrix()
+  {
+    Rows = new ROW[4];  // an array with 4 ROW structs:
+    NumRows = 4;
+
+    // initialize each row to have 4 columns:
+    for (int r = 0; r < NumRows; ++r)
+    {
+      Rows[r].Cols = new T[4];  // an array with 4 elements of type T:
+      Rows[r].NumCols = 4;
+
+      // initialize the elements to their default value:
+      for (int c = 0; c < Rows[r].NumCols; ++c)
+      {
+        Rows[r].Cols[c] = T{};  // default value for type T:
+      }
+    }
+  }
+
+  /*
+  * parameterized (overloaded) constructor:
+  *
+  * Called automatically by C++ to construct a matrix with R rows,
+  * where each row has C columns. All elements are initialized to
+  * the default value of T.
+  */
+  mymatrix(int R, int C)
+  {
+    if (R < 1)
+      throw invalid_argument("mymatrix::constructor: # of rows");
+    if (C < 1)
+      throw invalid_argument("mymatrix::constructor: # of cols");
+
+    /* Creating number of strucks which Rows is pointing to based on R*/
+    Rows = new ROW[R];
+    this->NumRows = R; //Updating the size of the rows.
+
+    for(int r = 0; r < this->NumRows; ++r)
+    {
+        Rows[r].Cols = new T[C]{}; //Initilized to T{} to prevent valgrind error. Note(3)
+        Rows[r].NumCols = C;
+    }
+  }
+
+
+  /*
+   * copy constructor(deep copy):
+   *
+   * Called automatically by C++ to construct a matrix that contains a
+   * copy of an existing matrix.  Example: this occurs when passing
+   * mymatrix as a parameter by value
+   *
+   *   void somefunction(mymatrix<int> M2)  <--- M2 is a copy:
+   *   { ... }
+   */
+  mymatrix(const mymatrix<T>& other)
+  {
+    this->Rows = new ROW[other.NumRows];
+    this->NumRows = other.NumRows;
+
+    for(int r = 0; r < other.NumRows; ++r)
+    {
+        this->Rows[r].NumCols = other.Rows[r].NumCols;
+        this->Rows[r].Cols = new T[other.Rows[r].NumCols]{}; //Initilized to T{} prevent valgrind errors. Note(3)
+
+        for(int c = 0; c < other.Rows[r].NumCols; ++c)
+        {
+            this->Rows[r].Cols[c] = other.Rows[r].Cols[c];
+        }
+    }
+  }
+
+
+  /**
+   * The function get us the current num of rows of the insatnce.
+   *
+   * @return the # of rows in the matrix.  The indices for these rows
+   *         are 0..numrows-1.
+  */
+  int numrows() const
+  {
+    return this->NumRows;
+  }
+  
+
+  /**
+   * Note that the # of columns can be different
+   * row-by-row since "jagged" rows are supported --- matrices are not
+   * necessarily rectangular.
+   *
+   * Restriction: The row has to be a valid already existing row.
+   *              The function does NOT create a new one and initialize
+   *              the num of Columns.
+   *
+   * @param r an integer value reflecting the row that to be accessed.
+   *
+   * @return the # of columns in row r. The indices for these columns
+   *         are 0..numcols-1.
+  */
+  int numcols(int r) const
+  {
+    /* Restriction */
+    if (r < 0 || r >= NumRows)
+      throw invalid_argument("mymatrix::numcols: row");
+
+    return this->Rows[r].NumCols;
+  }
+
+
+  /**
+   * Grows the # of columns in row r to at least C.  If row r contains
+   * fewer than C columns, then columns are added; the existing elements
+   * are retained and new locations are initialized to the default value
+   * for T.  If row r has C or more columns, then all existing columns
+   * are retained -- we never reduce the # of columns.
+   *
+   * Restrictions: 1. The row has to be one that already exists.
+   *               2. The number of columns to grow has to be more than
+   *                  one since the function does not additional rows.
+   *
+   * Jagged rows are supported, i.e. different rows may have different
+   * column capacities -- matrices are not necessarily rectangular.
+   *
+   * @param r the row at which the column to be grown.
+   * @param C the number of columns that we want a particular row to grow.
+   */
+  void growcols(int r, int C)
+  {
+    /* Restrictions */
+    if (r < 0 || r >= NumRows)
+      throw invalid_argument("mymatrix::growcols: row");
+    if (C < 1)
+      throw invalid_argument("mymatrix::growcols: columns");
+
+
+    /* Making sure we are not reducing the columns.
+     * The number of columns at the row have to be
+     * less than C in order for us to change them */
+    if(C <= numcols(r))
+    {
+      return;
+    }
+
+    /* Creating a new matrix which will have the regrown number of columns. */
+    T* regrownCol = new T[C]{}; //initiliazing to any type T{} in order to prevent valgrind issue. Note(3)
+
+    /* Copying over the elements to the new regrown column */
+    for (int c = 0; c < numcols(r); ++c)
+    {
+      regrownCol[c] = Rows[r].Cols[c];  // default value for type T:
+    }
+
+    /* Removing the old smaller sized column*/
+    delete[] Rows[r].Cols;
+
+    /* Mkaing the row to point at the new one. */
+    Rows[r].Cols = regrownCol;
+    Rows[r].NumCols = C; //Updating the size.
+  }
+
+
+  /**
+   * Grows the size of the matrix so that it contains at least R rows,
+   * and every row contains at least C columns.
+   * If the matrix contains fewer than R rows, then rows are added
+   * to the matrix; each new row will have C columns initialized to
+   * the default value of T.  If R <= numrows(), then all existing
+   * rows are retained -- we never reduce the # of rows.
+   * If any row contains fewer than C columns, then columns are added
+   * to increase the # of columns to C; existing values are retained
+   * and additional columns are initialized to the default value of T.
+   * If C <= numcols(r) for any row r, then all existing columns are
+   * retained -- we never reduce the # of columns.
+   *
+   * Cheacking whether there are already bigger size columns from the
+   * one passed  by the argument.
+   *
+   * New ROW pointer is created which is sized to the requster RxC.
+   * The elements from the old smaller matrix are copied over by a private
+   * helper function.
+   *
+   * @param R the number of row to grow to.
+   * @param C the number of columns to grow at each row.
+   */
+  void grow(int R, int C)
+  {
+    if (R < 1)
+      throw invalid_argument("mymatrix::grow: # of rows");
+    if (C < 1)
+      throw invalid_argument("mymatrix::grow: # of cols");
+
+    /* Handeling the case in which the number rows in the argument
+     * are less than or equal to the number of Rows. No new rows
+     * are created.
+     */
+    if( R < numrows())
+    {
+        for(int r = 0; r <= R; ++r)
+        {
+            growcols(r,C);
+        }
+        return;
+    }
+
+    ROW* regrownMatrix = new ROW[R];
+
+    int existingRows = numrows();
+    int columns;
+
+    /* Looping through all the rows that have been passed as an argument. */
+    for(int r = 0; r < R; ++r)
+    {
+        /* Have to be checking against the original array
+         * since there could be cases where there are more
+         * columns compared to what has been passed as an arg.
+         * For instance: C = 5 but this->Rows[r].NumCols = 10;
+         */
+        if ( r < existingRows)
+        {
+            /* Cheacking if # of columns is less than the
+             * columns in passed by the argument C at every row.
+             *
+             * If so then we know the columns need to be changed.
+             * columns = C;
+             * Otherwise, they will be kept the same number they have
+             * been before at that row.
+             */
+            if( numcols(r) < C){
+                columns = C;
+            }
+            else
+            {
+                columns = numcols(r);
+            }
+        }
+        /* If we there are more row than the one that exist then we
+         * know that we need to create a new row which will have the
+         * # of columens which is passed by the argument C.
+         */
+        else
+        {
+            columns = C;
+        }
+
+        //Allocatting the number of rows && columns in the temporary array.
+        regrownMatrix[r].Cols = new T[columns]{};
+        regrownMatrix[r].NumCols = columns;
+    }
+
+    /* Copying over the previous elements of the matrix to the temp matrix one with more spaces. */
+    copyMatrixVal(this->Rows, &regrownMatrix);
+
+    this->Rows = regrownMatrix;
+    this->NumRows = R;
+  }
+
+
+  /**
+   * The default size with no changes of an insatce of a matrix would be 16. (4x4)
+   *
+   * @return the total # of elements in the matrix.
+   */
+  int size() const
+  {
+    int totalElements = 0;
+
+    for(int r = 0; r < this->NumRows; ++r)
+    {
+        totalElements += Rows[r].NumCols;
+    }
+    return totalElements;
+  }
+
+
+  /**
+   *  M.at(r, c) = ...
+   *  cout << M.at(r, c) << endl;
+   *
+   * Restrictions: 1. The row has to be already existing one.
+   *               2. The column has to be already existing one.
+   *
+   * @param r the row where the element we want to access is
+   * @param c the column the element we want to access it at
+   *
+   * @return the addr of the element which are are trying to access,
+   *         allowing us to change it.
+   */
+  T& at(int r, int c)
+  {
+    /* Restrictions */
+    if (r < 0 || r >= NumRows)
+      throw invalid_argument("mymatrix::at: row");
+    if (c < 0 || c >= Rows[r].NumCols)
+      throw invalid_argument("mymatrix::at: col");
+
+    return this->Rows[r].Cols[c];
+  }
+
+
+  /**
+   *  M(r, c) = ...
+   *  cout << M(r, c) << endl;
+   *
+   * Restrictions: 1. The row has to be already existing one.
+   *               2. The column has to be already existing one.
+   *
+   * @param r the row where the element we want to access is
+   * @param c the column the element we want to access it at
+   * @return the addr to the element at the location,
+   *         allowing us to change it.
+   */
+  T& operator()(int r, int c)
+  {
+    /* Restrictions */
+    if (r < 0 || r >= NumRows)
+      throw invalid_argument("mymatrix::(): row");
+    if (c < 0 || c >= Rows[r].NumCols)
+      throw invalid_argument("mymatrix::(): col");
+
+    return this->Rows[r].Cols[c];
+  }
+
+
+  /**
+   * scalar multiplication
+   *
+   * Example: M2 = M1 * 2; (scalar = 2)
+   *
+   * Multiplies every element of this matrix by the given scalar value,
+   * producing a new matrix that is returned.  "This" matrix is not
+   * changed.
+   *
+   * @param scalar the value to multiply by.
+   * @return the instance which is a copy of the original
+   *         matrix and afterwards scaled.
+   */
+  mymatrix<T> operator*(T scalar)
+  {
+    /* Copying over the current matrix to a new instance. */
+    mymatrix<T> result = *this;
+
+      for (int r = 0; r < this->NumRows; ++r)
+      {
+          for (int c = 0; c < this->Rows[r].NumCols; ++c)
+          {
+              //Scaling
+              int matrixToMultiplyBy = this->Rows[r].Cols[c];
+              result.at(r,c) = matrixToMultiplyBy * scalar;
+          }
+      }
+    return result;
+  }
+
+
+  /**
+   * matrix multiplication
+   *
+   * Performs matrix multiplication M1 * M2, where M1 is "this" matrix and
+   * M2 is the "other" matrix.  This produces a new matrix, which is returned.
+   * "This" matrix is not changed, and neither is the "other" matrix.
+   *
+   * Example:  M3 = M1 * M2;
+   *
+   * Restrictions: M1 and M2 must be rectangular, if not an exception is thrown.  In
+   * addition, the sizes of M1 and M2 must be compatible in the following sense:
+   * M1 must be of size RxN and M2 must be of size NxC.  In this case, matrix
+   * multiplication can be performed, and the resulting matrix is of size RxC.
+   *
+   * @param other M2
+   * @return the multiplied M1xM2 matrix.
+   */
+  mymatrix<T> operator*(const mymatrix<T>& other)
+  {
+    int M1columns = numcols(0);
+    int M1rows = this->NumRows;
+    int M2columns = other.numcols(0);
+    int M2rows = other.NumRows;
+
+    /*  This loop is solely handling the columns */
+    for(int r = 0; r < this->NumRows; ++r)
+    {
+        /* Checking in case there is a row which has different number of columns
+         * The first column's size is saved and the rest are checked against it.
+         */
+        if(M1columns != numcols(r))
+        {
+            throw runtime_error("mymatrix::*: this not rectangular");
+        }
+        if(M2columns != other.numcols(r))
+        {
+            throw runtime_error("mymatrix::*: other not rectangular");
+        }
+        M1columns = numcols(r);
+        M2columns = other.numcols(r);
+    }
+
+    /* Checking if M! can be multiplied by M2.
+     * M1 columns must equal M2 rows        */
+    if (M1columns != M2rows)
+    {
+      throw runtime_error("mymatrix::*: size mismatch");
+    }
+
+    /*
+     * MULTIPLICATION
+     *
+     * r1 is the rows of M1.
+     * c2 is the columns of M2.
+     * c3 is the column of M1 and row of M2. (The one that match)
+     *
+     * Building the loop in such allowe us to first start with
+     * the row of M1 matrix. This row has to be multiplied by each
+     * column of M2/ Therefore that is the second inner loop.
+     * Lastly we go through each elemnt of the row and column
+     * which is the rowXcolumn match of M1 and M2.
+     */
+    mymatrix<T> result(M1rows, M2columns);
+    for(int r1 = 0; r1 < M1rows; ++r1)
+    {
+        for(int c2 = 0; c2 < M2columns; ++c2)
+        {
+            int answer = 0;
+            for(int c3 = 0; c3 <M2rows; ++c3)
+            {
+                answer += this->Rows[r1].Cols[c3] * other.Rows[c3].Cols[c2];
+            }
+            result.Rows[r1].Cols[c2] = answer;
+        }
+    }
+
+    return result;
+  }
+
+  /**
+   * Outputs the contents of the matrix; for debugging purposes.
+   */
+  void _output()
+  {
+    for (int r = 0; r < this->NumRows; ++r)
+    {
+      for (int c = 0; c < this->Rows[r].NumCols; ++c)
+      {
+        cout << this->Rows[r].Cols[c] << " ";
+      }
+      cout << endl;
+    }
+  }
+
+};
